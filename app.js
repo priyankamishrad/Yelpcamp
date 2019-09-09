47c8a35b98597cfee8dc75d47bcbe69d1cdc3ad2
Priyanka <priyanka83@gmail.com>
2019-09-09 11:07:46 -0500

    9.8.19 Updates

--- app.js
+++ app.js
@@ -5,13 +5,20 @@ const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const app = express();
 
-app.set('view engine','ejs');
 
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({ extended: true }))
 const languages = [
       {name : "Javascript", image: "https://img.icons8.com/nolan/2x/js.png"},
       {name : "Python", image: "https://img.icons8.com/color/2x/python.png"},
+      {name : "Swift", image: "https://img.icons8.com/nolan/2x/swift-payment-system.png"},
+      {name : "Javascript", image: "https://img.icons8.com/nolan/2x/js.png"},
+      {name : "Python", image: "https://img.icons8.com/color/2x/python.png"},
+      {name : "Swift", image: "https://img.icons8.com/nolan/2x/swift-payment-system.png"},
+      {name : "Javascript", image: "https://img.icons8.com/nolan/2x/js.png"},
+      {name : "Python", image: "https://img.icons8.com/color/2x/python.png"},
       {name : "Swift", image: "https://img.icons8.com/nolan/2x/swift-payment-system.png" }
-
 ];
 
 app.get('/',function(req,res){
@@ -20,7 +27,22 @@ app.get('/',function(req,res){
 
 app.get('/languages',function(req,res){
   res.render('languages',{languages:languages});
-})
+});
+
+app.get("/compose", function(req,res){
+  res.render('compose');
+  //console.log(req.body.addLang);
+});
+
+app.post("/compose", function(req,res){
+    let addLang = (req.body.addLang);
+    let image = (req.body.image);
+    let newLang = {addLang, image};
+    languages.push(newLang);
+    
+    res.redirect('/languages');
+    
+});
 
 app.listen(3000,function(){
   console.log("Server has started!");
