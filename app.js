100c7da9ebb2fc717a343133870e0f0367720c97
Priyanka <priyanka83@gmail.com>
2019-09-10 18:29:31 -0500

    9.10.19 Updates
    -Added database
    -connected form data to database
    -push and retrieve data from database

--- app.js
+++ app.js
@@ -3,45 +3,70 @@
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 
-
+mongoose.connect('mongodb://localhost/langdb',{useNewUrlParser: true,useUnifiedTopology: true });
 app.use(express.static("public"));
 app.set('view engine','ejs');
-app.use(bodyParser.urlencoded({ extended: true }))
-const languages = [
-      {name : "Javascript", image: "https://img.icons8.com/nolan/2x/js.png"},
-      {name : "Python", image: "https://img.icons8.com/color/2x/python.png"},
-      {name : "Swift", image: "https://img.icons8.com/nolan/2x/swift-payment-system.png"},
-      {name : "Javascript", image: "https://img.icons8.com/nolan/2x/js.png"},
-      {name : "Python", image: "https://img.icons8.com/color/2x/python.png"},
-      {name : "Swift", image: "https://img.icons8.com/nolan/2x/swift-payment-system.png"},
-      {name : "Javascript", image: "https://img.icons8.com/nolan/2x/js.png"},
-      {name : "Python", image: "https://img.icons8.com/color/2x/python.png"},
-      {name : "Swift", image: "https://img.icons8.com/nolan/2x/swift-payment-system.png" }
-];
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//schema set up
+const langSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+//set up model
+const Language = mongoose.model("Language",langSchema);
+
 
 app.get('/',function(req,res){
   res.render('landing');
 });
 
 app.get('/languages',function(req,res){
-  res.render('languages',{languages:languages});
+  //get languages from the database
+  Language.find({},function(err,allLangs){
+    if(err) {
+      console.log(err);
+    } else {
+      console.log(allLangs);
+      res.render('index',{languages:allLangs});
+    }
+  });
+
+  
 });
 
-app.get("/compose", function(req,res){
-  res.render('compose');
-  //console.log(req.body.addLang);
+app.get("/languages/new", function(req,res){
+  res.render('new');
 });
 
-app.post("/compose", function(req,res){
-    let addLang = (req.body.addLang);
-    let image = (req.body.image);
-    let newLang = {addLang, image};
-    languages.push(newLang);
-    
+app.post("/languages/new", function(req,res){
+    let addLang = (req.body.newlang);
+    let img = (req.body.newimg);
+    let desc = (req.body.newdesc);
+    let newLang = {
+      name : addLang,
+      image : img,
+      desc : desc
+    };
+
+    Language.create(newLang,function(err,added){
+      if(err) {
+        console.log(err);
+      } else {
+        console.log("Document inserted");
+      }
+    });
     res.redirect('/languages');
     
+});
+
+app.get("/languages/:id",function(req,res){
+  res.render('show');
 });
 
 app.listen(3000,function(){
